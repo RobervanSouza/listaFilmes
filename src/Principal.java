90ba7e6cc47150004760b4a01744775f03251118
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-17 11:35:01 -0300

    instanciando dados

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,11 @@
 public class Principal {
     public static void main(String[] args) {
-
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoLancamento = 1230;
+        meuFilme.duracao= 130;
+        System.out.println("Meu filme: " + meuFilme.nome);
+        System.out.println("Meu filme: " + meuFilme.anoLancamento);
+        System.out.println("Meu filme: " + meuFilme.duracao);
     }
 }
